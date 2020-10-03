d5e2ac6d855002075d6d43c3d946151617873782
zura0z <zuramazmishvili@gmail.com>
2020-10-03 11:46:53 +0400

    Updates

---
+++ precache-manifest.303f57731d093e342aac02daae35941f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7e9681915879b93723a419c57400bf",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "33c8421e0a7a2f005023",
+    "url": "/random-quote-machine/static/css/main.465fc3c2.chunk.css"
+  },
+  {
+    "revision": "8a3cf7162ed88f069ce4",
+    "url": "/random-quote-machine/static/js/2.6d7cf524.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.6d7cf524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c8421e0a7a2f005023",
+    "url": "/random-quote-machine/static/js/main.79085902.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
